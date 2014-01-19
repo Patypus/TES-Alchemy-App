0149adcc528dd1adce11fc5bf1ba8b7dff4464cd
Pat Howell <patrick.howell@outlook.com>
2014-01-19 21:35:59 +0000

    Initial commit of Application and Test projects

---
+++ TESAlchemyReference/gen/org/pat/howell/tes/alchemyreference/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.pat.howell.tes.alchemyreference;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ TESAlchemyReferenceTests/gen/org/pat/howell/tes/alchemyreference/test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.pat.howell.tes.alchemyreference.test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
