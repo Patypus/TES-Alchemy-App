849022143b8342453cb83e6dacc07af2136acde3
Pat Howell <patrick.howell@outlook.com>
2014-01-25 20:46:49 +0000

    TAA-1: Created database and resource file with table & column names in for use when accessing the database

--- TESAlchemyReference/gen/org/pat/howell/tes/alchemyreference/R.java
+++ TESAlchemyReference/gen/org/pat/howell/tes/alchemyreference/R.java
@@ -17,8 +17,8 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
@@ -33,9 +33,30 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int DatabaseName=0x7f040000;
+        public static final int EffectFourColumn=0x7f040007;
+        /**  Column titles 
+         */
+        public static final int EffectIdColumn=0x7f040009;
+        public static final int EffectOneColumn=0x7f040004;
+        public static final int EffectThreeColumn=0x7f040006;
+        public static final int EffectTitleColumn=0x7f04000a;
+        public static final int EffectTwoColumn=0x7f040005;
+        /**  Strings relating to the Effects table 
+ Table name 
+         */
+        public static final int EffectsTable=0x7f040008;
+        /**  Column titles 
+         */
+        public static final int IngredientIdColumn=0x7f040002;
+        public static final int IngredientNameColumn=0x7f040003;
+        /**  Strings relating to the Ingredients table 
+ Table name 
+         */
+        public static final int IngredientsTable=0x7f040001;
+        public static final int action_settings=0x7f04000c;
+        public static final int app_name=0x7f04000b;
+        public static final int hello_world=0x7f04000d;
     }
     public static final class style {
         /** 
